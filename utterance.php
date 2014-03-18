7c145702cd95eecc2d19cfe3b0a283e19b87176b
oelker <soelker@mtholyoke.edu>
2014-03-18 15:41:39 -0400

    more examples

---
+++ utterance.php
@@ -0,0 +1,7 @@
+ <?php
+$utterance = "I love you!";
+print ("When I'm happy, I want to say $utterance");
+print ("<p>");
+$utterance = "I will smite you!";
+print ("When I'm angry, I want to say $utterance");
+?>
